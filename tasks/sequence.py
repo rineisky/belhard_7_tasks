fc5a0e5c4e0082199047f8d5204b4e67ac973ed5
Konstantin Solovei <SoloveiKonstantin@gmail.com>
2022-02-02 18:03:36 +0300

    Create RandSequence class

--- tasks/sequence.py
+++ tasks/sequence.py
@@ -16,3 +16,23 @@
     random.randint(-n, n)
 - метод print_seq, который выводит последовательность на экран
 """
+
+import random
+
+
+class RandSequence:
+    n: int
+    sequence: list
+
+    def __init__(self, n: int) -> list:
+        self.n = n
+        self.sequence = [random.randint(-self.n, self.n) for _ in range(self.n)]
+
+    def generate(self, new_n: int = None) -> list:
+        if new_n is None:
+            self.sequence = [random.randint(-self.n, self.n) for _ in range(self.n)]
+        else:
+            self.sequence = [random.randint(-new_n, new_n) for _ in range(new_n)]
+
+    def print_seq(self):
+        print(self.sequence)
